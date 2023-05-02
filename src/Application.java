f7ba73f482988e77a648ccc6a30dd6ebb19ba09b
EndersteveGamer <85014963+EndersteveGamer@users.noreply.github.com>
2023-05-02 17:57:06 +0200

    Added connection opening

--- src/Application.java
+++ src/Application.java
@@ -8,4 +8,14 @@
 //Port number: 3306
 
 public class Application {
+    public static final String adresse = "sql7.freemysqlhosting.net";
+    public static final String bd = "sql7615548";
+    public static final String login = "sql7615548";
+    public static final String password = "pvudb2IQnY";
+
+    public static void main(String[] args) {
+        int connection = BD.ouvrirConnection(adresse, bd, login, password);
+
+
+    }
 }
