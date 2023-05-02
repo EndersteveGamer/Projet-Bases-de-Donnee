eaf5637be5c44c85b8be5a86329b995738cdf7f5
EndersteveGamer <85014963+EndersteveGamer@users.noreply.github.com>
2023-05-02 17:16:46 +0200

    Ajout des infos de la base de donnée

---
+++ src/Application.java
@@ -0,0 +1,11 @@
+// Sujet 2
+// Univ_fcomte.theop/noamf
+
+//Server: sql7.freemysqlhosting.net
+//Name: sql7615548
+//Username: sql7615548
+//Password: pvudb2IQnY
+//Port number: 3306
+
+public class Application {
+}
